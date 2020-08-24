1ae39260fb17a6c46d22d5f3eb620c865260f44c
banesullivan <bane.sullivan@kitware.com>
2020-08-24 12:21:18 -0600

    Improve UI controls

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ import vtkInteractorStyleManipulator from 'vtk.js/Sources/Interaction/Style/Inte
 import InteractionPresets from 'vtk.js/Sources/Interaction/Style/InteractorStyleManipulator/Presets';
 
 import controlPanel from './controller.html';
+import style from './controller.css'; // need to import for webpack to bundle it
 
 // ----------------------------------------------------------------------------
 // Standard scene code setup
@@ -34,11 +35,25 @@ fullScreenRenderer.getInteractor().setInteractorStyle(interactorStyle);
 InteractionPresets.applyDefinitions(interactorStyleDefinitions, interactorStyle);
 
 fullScreenRenderer.addController(controlPanel);
+const uiToggle = document.querySelector('.collapsible');
 const representationSelector = document.querySelector('.representations');
 const pointSizeChange = document.querySelector('.pointSize');
+const pointSizeRow = document.querySelector('.pointSizeRow');
 const lightingChange = document.querySelector('.lighting');
+const lightingRow = document.querySelector('.lightingRow');
 const ambientChange = document.querySelector('.ambient');
 const colorsChange = document.querySelector('.colors');
+
+// Set up UI hide/show toggler
+uiToggle.addEventListener('click', function () {
+  this.classList.toggle('active');
+  var content = this.nextElementSibling;
+  if (content.style.display === 'block') {
+    content.style.display = 'none';
+  } else {
+    content.style.display = 'block';
+  }
+});
 
 // ----------------------------------------------------------------------------
 // Rendering
@@ -48,6 +63,7 @@ const mapper = vtkMapper.newInstance();
 const actor = vtkActor.newInstance();
 
 representationSelector.addEventListener('change', (e) => {
+  // make sure this hides/shows the point size slider too
   let newRepValue = Number(e.target.value);
   if (newRepValue === 3) {
     actor.getProperty().setEdgeVisibility(1);
@@ -57,8 +73,26 @@ representationSelector.addEventListener('change', (e) => {
   }
   actor.getProperty().setRepresentation(newRepValue);
 
+  if (newRepValue === 0) {
+    // Points rep - show the slider for point size
+    pointSizeRow.style.display = 'block';
+    lightingRow.style.display = 'none';
+  } else {
+    // hide it
+    pointSizeRow.style.display = 'none';
+    lightingRow.style.display = 'block';
+  }
+
   renderWindow.render();
 });
+// if representationSelector is set to surface by default, then hide point slider
+if (Number(representationSelector.value) === 2) {
+  pointSizeRow.style.display = 'none';
+  lightingRow.style.display = 'block';
+} else {
+  pointSizeRow.style.display = 'block';
+  lightingRow.style.display = 'none';
+}
 
 pointSizeChange.addEventListener('input', (e) => {
   const pointSize = Number(e.target.value);
