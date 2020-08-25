fa337f4dbe9adc30a498cd567d0f2b146b373453
banesullivan <bane.sullivan@kitware.com>
2020-08-25 15:15:53 -0600

    Make interactor consistent wth ParaView

--- src/interactor.js
+++ src/interactor.js
@@ -3,8 +3,9 @@ import InteractionPresets from 'vtk.js/Sources/Interaction/Style/InteractorStyle
 
 function useVtkInteractorStyle (fullScreenRenderer) {
   const interactorStyleDefinitions = [
-    { type: 'pan', options: { button: 3 } }, // Pan on Right button drag
+    { type: 'pan', options: { button: 2 } }, // Pan on middle button drag
     { type: 'pan', options: { button: 1, shift: true } }, // Pan on Shift + Left button drag
+    { type: 'zoom', options: { button: 3 } }, // Zoom on right button drag
     { type: 'zoom', options: { button: 1, control: true } }, // Zoom on Ctrl + Left button drag
     { type: 'zoom', options: { dragEnabled: false, scrollEnabled: true } }, // Zoom on scroll
     { type: 'rotate', options: { button: 1 } } // Rotate on Left button drag
